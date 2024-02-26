3a4edbf57f6ebc9b78c3e16f25a061630611f82c
Abiolayoung <abiolayoung229@gmail.com>
2024-02-27 00:28:30 +0100

    update

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+    id : 1,
+    question: "what is mindlift wellness?",
+    answer: "mindlift wellness is an online thera-service provider"
+},
+{
+    id : 2,
+    question: "what is our mission statement?",
+    answer: "Empowering mental health through convenient online therapy sessions. Our mission is to make a positive impact on mental health by providing accessible and innovative virtual therapy services."
+},
+{
+    id : 3,
+    question: "what are the services we offer?",
+    answer: "At Mindlift Wellness, we offer a comprehensive range of mental health care services tailored to meet the diverse needs of our clients. Our services include: Online therapy sessions, individual therapy, group therapy, crisis intervention, virtual workshops and seminars"
+},
+{
+    id : 4,
+    question: "how much does a service cost?",
+    answer: "our service charge ranges from $30-60 weekly for an average therapy sessions"
+},
+{
+    id : 5,
+    question: "how many sessions are offered for teraphy service?",
+    answer: "Our theraphy sessions with clients is a 3 days weekly"
+},
+];
+
+export default data;
